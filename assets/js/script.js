55c4aca0f53c40fcd2edbc0e66861cb936d1a816
Mivic1998 <maxivic1998@gmail.com>
2026-05-31 20:28:59 +0100

    Adjusted Layout for Mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,7 +213,7 @@ for (let slider of sliders) {
     // when user finishes adjusting (releases the slider), scroll to the graph
    /* slider.addEventListener('change', () => {
         mainSection.scrollIntoView({ behavior: 'smooth' });
-    })*/;
+    });*/
 }
 
 demandType.addEventListener("change", (e) => {
